0ff1abcc2920472469202a45229714b9e295f508
Seoungju Oh <osj4532@daum.net>
2019-12-11 17:35:49 +0900

    add basic syntax

---
+++ javaScriptStudy/syntax/templateLiteral.js
@@ -0,0 +1,10 @@
+var name = 'osj';
+var letter = `Esse culpa  ${name} 
+ipsum magna est nulla. Officia quis cupidatat exercitation 
+anim nostrud laboris mollit sint pariatur laborum 
+reprehenderit aliquip sit. Cupidatat tempor cillum cupidatat 
+veniam voluptate ex minim magna ${name} ullamco ea excepteur officia. 
+Eu anim culpa est ex aute ex consectetur irure non commodo occaecat 
+nisi dolore. Consectetur cillum irure esse ${name} esse nulla velit esse ea 
+pariatur culpa magna in.`;
+console.log(letter);
---
+++ javaScriptStudy/syntax/variable.js
@@ -0,0 +1,8 @@
+var a = 1;
+console.log(a);
+a = 2;
+console.log(a);
+// 1 = 2 comment;
+var name = 'osj';
+var letter = 'Esse culpa ' + name + ' ipsum magna est nulla. Officia quis cupidatat exercitation anim nostrud laboris mollit sint pariatur laborum reprehenderit aliquip sit. Cupidatat tempor cillum cupidatat veniam voluptate ex minim magna ullamco ea excepteur officia. Eu anim culpa est ex aute ex consectetur irure non commodo occaecat nisi dolore. Consectetur cillum irure esse esse nulla velit esse ea pariatur culpa magna in.'
+console.log(letter);
