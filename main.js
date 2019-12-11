9808285a3ed125e1858b3cdc5161f4c6f9987865
Seoungju Oh <osj4532@daum.net>
2019-12-11 20:35:34 +0900

    add simple 404 error page

--- main.js
+++ main.js
@@ -6,41 +6,39 @@ var app = http.createServer((request, response) => {
     var _url = request.url;
     var queryData = url.parse(_url, true).query;
     var title = queryData.id;
-    console.log(queryData);
-    if(_url == '/'){
-        title = 'Welcome';
+    var pathname = url.parse(_url, true).pathname;
+
+    if(pathname === '/'){
+      fs.readFile(`data/${title}`, 'utf8', (err, data)=>{
+        var description = data;
+        var template = `
+        <!doctype html>
+        <html>
+        <head>
+          <title>WEB1 - ${title}</title>
+          <meta charset="utf-8">
+        </head>
+        <body>
+          <h1><a href="/">WEB</a></h1>
+          <ol>
+            <li><a href="/?id=HTML">HTML</a></li>
+            <li><a href="/?id=CSS">CSS</a></li>
+            <li><a href="/?id=JavaScript">JavaScript</a></li>
+          </ol>
+          <h2>${title}</h2>
+          <p>${description}</p>
+        </body>
+        </html>
+        `;
+        response.writeHead(200);
+        response.end(template);
+      });
+    }else{
+      response.writeHead(404);
+      response.end('Not Found');
     }
 
-    if(_url == '/favicon.ico'){
-        response.writeHead(404);
-        response.end();
-        return;
-    }
-
-    response.writeHead(200);
-    fs.readFile(`data/${title}`, 'utf8', (err, data)=>{
-      var description = data;
-      var template = `
-    <!doctype html>
-    <html>
-    <head>
-      <title>WEB1 - ${title}</title>
-      <meta charset="utf-8">
-    </head>
-    <body>
-      <h1><a href="/">WEB</a></h1>
-      <ol>
-        <li><a href="1.html?id=HTML">HTML</a></li>
-        <li><a href="2.html?id=CSS">CSS</a></li>
-        <li><a href="3.html?id=JavaScript">JavaScript</a></li>
-      </ol>
-      <h2>${title}</h2>
-      <p>${description}</p>
-    </body>
-    </html>
-    `;
-    response.end(template);
-    });
+    
     
     
 });
